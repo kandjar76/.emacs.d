91b93f434b21ab707787646af427bfc687a9e90f
Cedric Lallain <kandjar@camelia>
2008-10-27 01:52:57 -0700

    [mtpchat] Refactorisation part 2: fontification/todolist/autologin/timestamp

--- personal/mtpchat.el
+++ personal/mtpchat.el
@@ -3,6 +3,16 @@
 ;;
 
 
+;; TODO:
+;; -- knowing who's connected
+;; -- setup title bar (user/channel/topic/away?/nb users/...)
+;; -- notification in the main line
+;; -- private tell window
+;; -- button --> finger on user // url!
+;; -- show-msg/wall more coloring!
+;; -- autocompletion of nicks!
+;; -- accent!!!!!
+
 (require 'tcp-client)
 
 ;;
@@ -14,11 +24,9 @@
 
 (defvar mtpchat--marker nil 
   "Marker for mtpchat")
-(make-variable-buffer-local 'mtpchat--marker)
 
 (defvar mtpchat--incomplete-line-save nil 
   "Store the incomplete line here waiting for the rest")
-(make-variable-buffer-local 'mtpchat-newline-insert)
 
 (defvar mtpchat--history nil 
   "Input history")
@@ -29,10 +37,71 @@
 				  :keep-alive t)
   "MtpChat Connection Settings")
 
+(defvar mtpchat--login nil
+  "MtpChat Login name")
+(defvar mtpchat--passwd nil
+  "MtpChat Password")
+
 (defvar mtpchat--input-prefix "MTP> "
   "Prompt for entering a mtpchat text...")
-
 (defvar mtpchat--input-start-marker nil)
+
+;;
+;; Fonts:
+;;
+
+;; Create faces for various opcode classes.
+(make-face 'mtpchat-prompt-face)
+(set-face-background 'mtpchat-prompt-face "slategray1")
+(make-face-bold 'mtpchat-prompt-face)
+(defvar mtpchat-prompt-face 'mtpchat-prompt-face
+  "Font to highlight mtp prompt.")
+
+(make-face 'mtpchat-time-stamp-face)
+(set-face-foreground 'mtpchat-time-stamp-face "limegreen")
+(defvar mtpchat-time-stamp-face 'mtpchat-time-stamp-face
+  "Font to highlight mtp timestamp.")
+
+(make-face 'mtpchat-mtp-face)
+(set-face-foreground 'mtpchat-mtp-face "red")
+(defvar mtpchat-mtp-face 'mtpchat-mtp-face
+  "Font to highlight mtp messages.")
+
+(make-face  'mtpchat-emacs-face)
+(set-face-foreground 'mtpchat-emacs-face "cornflowerblue")
+(defvar mtpchat-emacs-face 'mtpchat-emacs-face
+  "Font to highlight emacs messages.")
+
+(make-face  'mtpchat-your-line-face)
+(set-face-foreground 'mtpchat-your-line-face "blue")
+(defvar mtpchat-your-line-face 'mtpchat-your-line-face
+  "Font to highlight a mtp chat line you sent.")
+
+(make-face  'mtpchat-your-name-face)
+(set-face-foreground 'mtpchat-your-name-face "darkslateblue")
+(defvar mtpchat-your-name-face 'mtpchat-your-name-face
+  "Font to highlight a mtpchat line with your name in it.")
+
+(make-face 'mtpchat-you-tell-face)
+(set-face-foreground 'mtpchat-you-tell-face "cadet blue")
+(defvar mtpchat-you-tell-face 'mtpchat-you-tell-face
+  "Font to highlight a private msg you sent on mtpchat.")
+
+(make-face 'mtpchat-private-tell-face)
+(set-face-foreground 'mtpchat-private-tell-face "forest green")
+(defvar mtpchat-private-tell-face 'mtpchat-private-tell-face
+  "Font to highlight a private msg you received on mtpchat.")
+
+(make-face 'mtpchat-emote-face)
+(set-face-foreground 'mtpchat-emote-face "deep pink")
+(defvar mtpchat-emote-face 'mtpchat-emote-face
+  "Font to highlight emote messages.")
+
+(make-face 'mtpchat-away-face)
+(set-face-foreground 'mtpchat-away-face "LavenderBlush4")
+(defvar mtpchat-away-face 'mtpchat-away-face
+  "Font to highlight away messages.")
+
 
 ;;
 ;; Network handlers:
@@ -40,6 +109,8 @@
 
 (defun mtpchat--connection-established(buffer server port)
   (setq mtpchat--incomplete-line-save nil) ;; new connection -- no incomplete lines...
+  (with-current-buffer buffer 
+    (add-hooks 'mtpchat--validate-message-hook 'mtpchat--auto-login))
   (mtpchat--insert 'mtpchat-system (format "<Emacs> Connection established to %s:%i\n" server port)))
 
 (defun mtpchat--connection-abort(buffer server port)
@@ -53,6 +124,94 @@
 
 (defun mtpchat--filter(process message)
   (mtpchat--insert-data message))
+
+
+;;
+;; Fontification structure:
+;;
+
+(setq mtpchat--data-finder 
+      '(
+	(mtpchat-mtp-face          'login             "<Mtp> Login")
+	(mtpchat-mtp-face          'alreadylog        "<Mtp> You are already logged in, try with another login name")
+	(mtpchat-mtp-face          'invalidpass       "<Mtp> Incorrect password")
+		           
+	(nil                       'welcome           "Welcome to ")
+	(mtpchat-mtp-face          'away              "<Mtp> You are away")
+	(mtpchat-mtp-face          'back              "<Mtp> You are back")
+	(mtpchat-away-face         'tellaway          "<Mtp> (/w+):(nick) is away and may not be hearing you")
+	(mtpchat-mtp-face          'useraway          "<Mtp> (/w+):(nick) is away")
+	(mtpchat-mtp-face          'userback          "<Mtp> (/w+):(nick) is back")
+	(mtpchat-mtp-face          'wellogin          "<Mtp> Welcome, (/w+):(login)/.")
+		           
+	(mtpchat-emote-face        'emote             "<Mtp> /*(/w+):(nick) (.+):(text)$")
+	(mtpchat-mtp-face          'shout             "<Mtp> (/w+):(nick) shouts: (.+):(text)$")
+	(mtpchat-mtp-face          'youshout          "<Mtp> You shout: (.+):(text)$")
+	(mtpchat-you-tell-face     'youtell           "<Mtp> You {tell|ask|reply to} (/w+):(nick): (.+):(text)$")
+	(mtpchat-private-tell-face 'tell              "<Mtp> (/w+):(nick) {{tell|ask}s you|replies}: (.+):(text)$")
+	(mtpchat-mtp-face          'kicked            "<Mtp> (/w+):(nick) is kicked out by (/w+):(text)/b")
+	(mtpchat-mtp-face          'kick              "<Mtp> You kick (/w+):(nick)/b")
+	(mtpchat-mtp-face          'topjoin           "<Mtp> (/w+):(channel) topic : (.*):(topic)$")
+	(mtpchat-mtp-face          'topicset          "<Mtp> (/w+):(nick) set channel (/w+):(channel) topic to (.*):(topic)$")
+	
+	(mtpchat-mtp-face          'comefrom          "<Mtp> (/w+):(nick) {comes in from channel|appears from the shadows}")
+	(mtpchat-mtp-face          'leavechannel      "<Mtp> (/w+):(nick) left channel")
+	(mtpchat-mtp-face          'fadechannel       "<Mtp> (/w+):(nick) fades into the shadows")
+	(mtpchat-mtp-face          'joinedchannel     "<Mtp> (/w+):(nick) joined channel (/w+):(channel)/b")
+	(mtpchat-mtp-face          'join              "<Mtp> You join channel (/w+):(channel)/b")
+	(mtpchat-mtp-face          'leave             "<Mtp> You leave channel")
+	
+	(mtpchat-mtp-face          'comein            "<Mtp> (/w+):(nick) comes in")
+	(mtpchat-mtp-face          'leaves            "<Mtp> (/w+):(nick) {leaves|disconnects}")
+	
+	(mtpchat-mtp-face          'bump              "<Mtp> {(/w+):(nick) is bumped|You bump (/w+):(nick) out}")
+	(mtpchat-mtp-face          'bumped            "<Mtp> You are bumped out by (/w+):(nick)/b")
+	(xxx                       'usrbeep           "<Mtp> (/w+):(nick) beeps you")
+	(xxx                       'youbeep           "<Mtp> You beep (/w+):(nick)/b")
+	
+	(mtpchat-mtp-face          'msgbegin          "<Mtp> Your message/(s/)")
+	(mtpchat-mtp-face          'msgend            "<Mtp> You have (/d+|no):(nbmsgs) message")
+	
+	(xxx                       'msgline           " ?(/d+):(numeric) (/d/d///d/d///d/d /d/d:/d/d:/d/d):(msgtime) (/w+):(nick) : (.*):(text)$")
+	(xxx                       'wallline          "(/d/d///d/d///d/d /d/d:/d/d:/d/d|/w/w/w /w/w/w /d/d /d/d/d/d /d/d:/d/d:/d/d):(msgtime) (/w+):(nick) (.*):(text)$")
+		           
+	(mtpchat-mtp-face          'whobegin          " ?Login +Group +Channel +Idle +On +For +C +From host")
+	(nil                       'wholine           "(/w+):(name) +(/w+):(group) +([a-zA-Z0-9]+):(channel) +([/*0-9][0-9A-Za-z/*]+):(idle) +[^ ]+ +(.):(client)")
+	(mtpchat-mtp-face          'whoend            "<Mtp> There {are|is} currently /d+ users?")
+	
+	(mtpchat-mtp-face          'finger            "Login +: (/w+):(nick)")
+	(mtpchat-mtp-face          'sysalias          "<Mtp> System Aliases")
+	(mtpchat-mtp-face          'usralias          "<Mtp> User Aliases")
+	(mtpchat-mtp-face          'history           "<Mtp> History")
+	(mtpchat-mtp-face          'uptime            "<Mtp> Uptime")
+	(mtpchat-mtp-face          'wall              "<Mtp> Wall")
+	(mtpchat-mtp-face          'helpfor           "<Mtp> Help for ")
+	(mtpchat-mtp-face          'infobeg           "{ Login|Channel| Group|Command}")
+	(nil                       'infosep           "-")
+	(mtpchat-mtp-face          'infoend           "<Mtp> {There|End of}")
+	(mtpchat-mtp-face          'you                       "<Mtp> You")
+	(mtpchat-mtp-face          'getpass           "<Mtp> Password")
+	(mtpchat-mtp-face          'servermoved       "<Mtp> Main server moved at ([^:]+):(server):(/d+):(port)/b")
+	(mtpchat-mtp-face          'badserver         "<Mtp> Active server is at ([^:]+):(server):(/d+):(port)/b")
+	(xxx                       'notloggedin       "<Mtp> User (/w+):(nick) is not logged in !")
+	(mtpchat-mtp-face          'mtpdatas          "<Mtp> ")
+	(mtpchat-emacs-face        'mtpdatas          "<Emacs> ")
+	
+	(xxx                       'kfilesendserver   "/|(/w+):(nick)/| /[Rainbow/]/[Send/]/[(.*):(filename)/]/[(/d+):(size)/]/[(/d+):(id)/]/[(.*):(ip)/]/[(/d+):(port)/]/[(.*):(password)/](.*):(text)$" )
+	(xxx                       'kfilesend         "/|(/w+):(nick)/| /[Rainbow/]/[Send/]/[(.*):(filename)/]/[(/d+):(size)/]/[(/d+):(id)/](.*):(text)$" )
+	(xxx                       'kfileacceptserver "/|(/w+):(nick)/| /[Rainbow/]/[Accept/]/[(/d+):(id)/]/[(.*):(ip)/]/[(/d+):(port)/]/[(.*):(password)/](.*):(text)$" )
+	(xxx                       'kfileaccept       "/|(/w+):(nick)/| /[Rainbow/]/[Accept/]/[(/d+):(id)/](.*):(text)$" )
+	(xxx                       'kfilerefuse       "/|(/w+):(nick)/| /[Rainbow/]/[Refuse/]/[(/d+):(id)/](.*):(text)$" )
+	
+	(xxx                       'ksenddatadrawline "/|(/w+):(nick)/| /}[lL] (/d+):(color) (/d+):(x1) (/d+):(y1) (/d+):(x2) (/d+):(y2) (/d+):(width)")
+	(xxx                       'kchanneldrawline  "<(/w+)> /}[lL] (/d+):(color) (/d+):(x1) (/d+):(y1) (/d+):(x2) (/d+):(y2) (/d+):(width)")
+	
+	(xxx                       'kdatas            "/|(/w+):(nick)/| (....):(request) (.*):(data)$" )
+	(xxx                       'senddata          "/|(/w+):(nick)/| (.*):(text)$" )
+	
+	(nil                       'sentence          "<(/w+):(nick)> (.*):(text)$")
+	(nil                       'block             "[^<0-9]")
+	))
 
 
 ;;
@@ -94,7 +253,6 @@ appearance and content of the inserted text"
 text is finalized. No more modification should be done here."
   :group 'mtpchat-group
   :type 'hook)
-;; read-only
 ;; modeline track
 
 ;; (set-window-dedicated-p window t)
@@ -103,6 +261,8 @@ text is finalized. No more modification should be done here."
   "Hook run after `mtpchat-mode' setup is complete."
   :group 'mtpchat-group
   :type 'hook)
+
+
 
 ;;
 ;; Utility functions:
@@ -167,6 +327,68 @@ supported:
       (tcp-send (get-buffer-process mtpchat--main-buffer-name) (concat to-send "\n"))
       )))
 
+
+(defun mtpchat--remove-away-time()
+  (goto-char (point-min))
+  (when (looking-at "^\d\d:\d\d:\d\d ")
+    (delete-char 9)))
+
+(defun mtpchat--add-local-time()
+  (goto-char (point-min))
+  (let ((curtime (current-time))
+	(fts (format-time-string "[%H:%M:%S]" (current-time))))
+    (insert fts)
+    (add-text-properties (point-min) (point)
+			 '(face mtpchat-time-stamp-face))
+    (insert " ")
+    (add-text-properties (point-min) (point)
+			 '(intangible t front-sticky t rear-nonsticky t))
+))
+  
+
+(defun mtpchat--auto-login(type msg)
+  (when (and (string-match "^<Mtp> Login: $" msg)
+	     mtpchat--login)
+    (tcp-send (get-buffer-process mtpchat--main-buffer-name) (concat mtpchat--login "\n")))
+  (when (and (string-match "^[^<]*<Mtp> Password: $" msg)
+	     mtpchat--passwd)
+    (tcp-send (get-buffer-process mtpchat--main-buffer-name) (concat mtpchat--passwd "\n")))
+  (when (string-match "^<Mtp> Welcome, " msg)
+    (tcp-send (get-buffer-process mtpchat--main-buffer-name) "set client zzz .o(v0.2)\n")
+    (remove-hook 'mtpchat--validate-message-hook 'mtpchat--auto-login)))
+
+
+
+(defun mtpchat--fontify ()
+  (goto-char (point-min))
+  (cond ((looking-at "^<Mtp> You \\(tell\\|ask\\|reply\\)") 
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-you-tell-face)))
+	
+	((looking-at "^<Mtp> \\w+ \\(tells\\|asks\\|replies\\)") 
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-private-tell-face)))
+
+	((looking-at "^<Mtp> \\*")
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-emote-face)))
+
+	((looking-at "^<Mtp> \\w+ is away and may not be hearing you")
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-away-face)))
+
+	((looking-at "^<Mtp>")
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-mtp-face)))
+
+	((looking-at "^<Emacs>")
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-emacs-face)))
+
+	((looking-at "^<Emacs>")
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-emacs-face)))
+
+	((looking-at "^<Kandjar>")
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-your-line-face)))
+
+	((looking-at "kandjar")
+	 (add-text-properties (point-min) (point-max) '(face mtpchat-your-name-face)))
+   ))
+
 ;; Define the key mapping for the mtpchat mode:
 (defvar mtpchat-mode-map
   (let ((mtpchat-mode-map (make-keymap)))
@@ -184,7 +406,7 @@ supported:
 ;	(cons (concat "^<" mtpchat-login ">.*$" ) 'mtpchat-your-line-face)
 ;	(cons mtpchat-login 'mtpchat-your-name-face))
 ;  "Additional expressions to highlight in MtpChat lines mode.")
-;
+
 
 (defun mtpchat--scroll-to-bottom (window display-start)
   "Scroll to the bottom of the display when the user is typing some texts...
@@ -211,10 +433,13 @@ Function added to `window-scroll-functions' by mtpchat-mode"
 
   ;; Buffer Local
   (make-variable-buffer-local 'mtpchat--incomplete-line-save)
+  (make-variable-buffer-local 'mtpchat--marker)
+  (make-variable-buffer-local 'mtpchat--input-start-marker)
 
   ;; Get rid of the ^M at the end of the lines:
   (setq buffer-display-table (make-display-table))
   (aset buffer-display-table ?\r [])
+  ;(aset buffer-display-table ?\240 ?\a)
 
   ;; Marker creation:
   (setq mtpchat--marker (make-marker))
@@ -235,6 +460,9 @@ Function added to `window-scroll-functions' by mtpchat-mode"
   ;; Set the prompt:
   (insert (or prompt mtpchat--input-prefix))
   (add-text-properties (marker-position mtpchat--marker)
+		       (1- (point-max))
+		       '(face mtpchat-prompt-face))
+  (add-text-properties (marker-position mtpchat--marker)
 		       (point-max)
 		       '(read-only t intangible t rear-nonsticky t mtp-prompt t front-sticky t))
  
@@ -249,6 +477,11 @@ Function added to `window-scroll-functions' by mtpchat-mode"
   (make-local-hook 'mtpchat--modify-hook)
   (make-local-hook 'mtpchat--validate-message-hook)
 
+  (add-hook 'mtpchat--validate-message-hook 'mtpchat--auto-login)
+
+  (add-hook 'mtpchat--modify-hook 'mtpchat--remove-away-time)
+  (add-hook 'mtpchat--modify-hook 'mtpchat--fontify)
+  (add-hook 'mtpchat--modify-hook 'mtpchat--add-local-time t)
   (add-hook 'mtpchat--post-insert-hook 'mtpchat--make-read-only)
   ;; 
 
@@ -264,7 +497,14 @@ Function added to `window-scroll-functions' by mtpchat-mode"
 					:connection-abort-handler 'mtpchat--connection-abort
 					:connection-failed-handler 'mtpchat--connection-failed
 					:sentinel-handler 'mtpchat--sentinel
-					:filter-handler 'mtpchat--filter)))
+					:filter-handler 'mtpchat--filter))
+	(server (read-string "Server: " (get-tcp-connection-server mtpchat--connection) nil  
+			     (get-tcp-connection-server mtpchat--connection)))
+	(login (read-string "Login: " (user-login-name)))
+	(pwd   (read-passwd "Password: ")))
+    (set-tcp-connection-server mtpchat--connection server)
+    (setq mtpchat--login  login)
+    (setq mtpchat--passwd pwd)
     (get-buffer-create mtpchat--main-buffer-name)
     (save-excursion 
       (set-buffer mtpchat--main-buffer-name)
