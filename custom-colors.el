a254e864f48143406aecb81b7f297b1c77909d70
Cedric Lallain <kandjar@nausicaa>
2008-11-18 12:29:44 -0800

    [color] Fix some color issue + isearch background color modified

--- custom-colors.el
+++ custom-colors.el
@@ -5,20 +5,6 @@
 (set-face-background 'bm-face      (first-valid-color "lightcyan" "blue"))
 (set-face-foreground 'bm-face      (first-valid-color "black" "blue"))
 
-;; Highlight the current line:
-(set-face-background 'highlight-current-line-face (first-valid-color "#C0E0FF" "lightblue" "white"))
-
-;; I-Search:
-(set-face-foreground 'isearch (first-valid-color "red"))
-(set-face-background 'isearch (first-valid-color "lightgray" "grey" "white"))
-
-;; Color of the text selection:
-(set-face-background 'region (first-valid-color "#FFE0C0" "brightcyan"))
-
-
-
-
-
 
 ;; ???
 (set-face-foreground 'font-lock-builtin-face   (first-valid-color "darkcyan" "violetred" "darkmagenta" "magenta"))
@@ -56,6 +42,16 @@
 
 ;; Color of the language keyword: const, if, else...
 ;(set-face-foreground 'font-lock-keyword-face (first-valid-color "blue"))
+
+;; Highlight the current line:
+;(set-face-background 'highlight-current-line-face (first-valid-color "#C0E0FF" "lightblue" "white"))
+
+;; I-Search"
+;(set-face-foreground 'isearch (first-valid-color "red"))
+;(set-face-background 'isearch (first-valid-color "gray" "grey" "white"))
+
+;; Color of the text selection:
+;(set-face-background 'zmacs-region (first-valid-color "#FFE0C0" "brightcyan"))
 
 ;; Color of the cursor:
 ;(set-face-background 'text-cursor (first-valid-color "red"))
