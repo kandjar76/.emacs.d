89a0f89626ebda6871366d43812e8d4ffadf0c74
Cedric Lallain <kandjar@nausicaa>
2008-11-18 22:44:33 -0800

    [modes] Small fixes to the Awk script
      - Remove the binding to the special delete function -- pending-delete-mode does the job
      - Small syntax table fixes

--- modes/my-awk.el
+++ modes/my-awk.el
@@ -6,7 +6,7 @@
   "Intelligently decides whether to indent or do word-completion."
   (interactive "P")
   ;; Special case: a region has been selected -- run the alignment on the selected region
-  (if (region-active-p)
+  (if mark-active
       (c-indent-line-or-region)
       (c-indent-command ARG))
 )
@@ -15,7 +15,6 @@
   ;;(c-set-style "gnu")
   (setq tab-width 4)
   (local-set-key [(tab)] 'my-awk-smart-tab)
-  (local-set-key [(delete)] 'special-delete-for-region)
   (c-set-offset 'substatement 4 nil)
   (c-set-offset 'statement-block-intro 4 nil)
   (c-set-offset 'substatement-open 0 nil)
@@ -24,6 +23,8 @@
   (local-set-key [(control ?x) (?a) (?c)] 'awk-align-comment)
   (local-set-key [(control ?q)]           'awk-comment-block)
   (modify-syntax-entry ?_ "w"   awk-mode-syntax-table)
+  ;(modify-syntax-entry ?\# "< b"   awk-mode-syntax-table)
+  ;(modify-syntax-entry ?\n "> b"   awk-mode-syntax-table)
 )
 
 (add-hook 'awk-mode-hook 'my-awk-setup)
