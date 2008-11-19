3dd4acfce359a1551d40abf90361d7ed44a49ae9
Cedric Lallain <kandjar@nausicaa>
2008-11-18 23:52:29 -0800

    [libs] Update klog expected format + Start the implementation

--- libs/klog.el
+++ libs/klog.el
@@ -5,7 +5,7 @@
 ;; klog-header( title)
 ;; klog-printf(type format ...)
 ;; klog-push(type format ...)
-;; klog-pop(type)
+;; klog-pop()
 ;; klog-new-line(type ...)
 ;; 
 ;; A log file will look like:
@@ -18,21 +18,50 @@
 ;;  [errs] `- a std error
 ;;  [crit] `- a major issue
 ;;  [info] `- entering a block
-;;  [info] | `- log within this block
-;;  [info] | `- second log within the same block
+;;  [info] |   `- log within this block
+;;  [info] |   `- second log within the same block
 ;;  [errs] `- a std error
 ;;  [crit] `- a major issue
 ;;  [info] `- entering a block
-;;  [info] | `- log within this block
-;;  [info] | `- second log within the same block
-;;  [info] | `- second log within the same block
-;;  [info] | | `- second log within the same block
-;;  [info] | | `- second log within the same block
-;;  [info] | | `- second log within the same block
-;;  [info] | `- second log within the same block
+;;  [info] |   `- log within this block
+;;  [info] |   `- second log within the same block
+;;  [info] |   `- second log within the same block
+;;  [info] |   |   `- second log within the same block
+;;  [info] |   |   `- second log within the same block
+;;  [info] |   |   `- second log within the same block
+;;  [info] |   `- second log within the same block
 ;;  [todo] `- A todo item
 ;;  [note] `- Just a comment
 
+;;  [info] `- first log entry 
+;;  [info] `- second log entry
+;;  [warn] `- small issue
+;;  [errs] `- a std error
+;;  [crit] `- a major issue
+;;  [info] `- entering a block
+;;  [info] |   `- log within this block
+;;  [info] |   `- second log within the same block
+;;  [errs] `- a std error
+;;  [crit] `- a major issue
+;;  [info] `- entering a block
+;;  [info] |   `- log within this block
+;;  [info] |   `- second log within the same block
+;;  [info] |   `+ second log within the same block
+;;  [info] |   `- second log within the same block
+;;  [todo] `- A todo item
+;;  [note] `- Just a comment
 
-;(defun klog-header(buffer title)
-;  (
+(require 'log-buffer)
+(require 'enum-type)
+
+(defun make-new-klog-buffer (buffer-name)
+  "Create a klog buffer, called BUFFER-NAME
+If BUFFER-NAME already exist, this function will set the read-only flag, 
+and setup the klog mode for this buffer"
+  (let ((klog-buffer (make-new-log-buffer buffer-name)))
+    (set-buffer klog-buffer)
+    (klog-mode)))
+
+
+;(make-new-klog-buffer "*klog-test*")
+
