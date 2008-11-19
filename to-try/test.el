b07cc72e9caa4f10289e79688719101b181c2cdf
Cedric Lallain <kandjar@nausicaa>
2008-11-19 14:32:49 -0800

    [to-try] Update test.el

--- to-try/test.el
+++ to-try/test.el
@@ -1,2 +1,6 @@
 
 (completing-read "Completion of foo:" '(("foobar1" 1)("barfoo" 2) ("foobas" 3)("foobar2" 4)) nil t "foo" )
+
+(move-to-window-line 0)  ;; Move to the first line of the window
+(move-to-window-line -1) ;; Move to the last line of the window
+
