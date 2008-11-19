9b47ab65a28968e80958057cb6f2d235d71444a4
Cedric Lallain <kandjar@nausicaa>
2008-11-18 21:29:25 -0800

    [keys] Fix emacs key mapping

--- custom-keys.el
+++ custom-keys.el
@@ -9,7 +9,7 @@
 (define-key global-map [(control meta g)]        	'goto-line)
 (define-key global-map [(insert)]                	'overwrite-mode)
 
-(define-key global-map [(control insert)]               'copy-region-as-kill) ; kill-ring-save -- will add a delay to show the "copied" area
+(define-key global-map [(control insert)]               'kill-ring-save)
 (define-key global-map [(control delete)]        	'kill-region)
 (define-key global-map [(shift insert)]                 'yank)
 (define-key global-map [(shift delete)]          	'yank-pop)
@@ -26,9 +26,9 @@
 (define-key global-map [(meta left)]                    'backward-sexp)
 (define-key global-map [(meta right)]                   'forward-sexp)
 
-(define-key global-map [(mouse-1)]               	'mouse-track)
-(define-key global-map [(mouse-2)]               	nil)
-(define-key global-map [(mouse-3)]               	nil) ; 'popup-mode-menu)
+(define-key global-map [(button1)]               	'mouse-track)
+(define-key global-map [(button2)]               	nil)
+(define-key global-map [(button3)]               	'popup-mode-menu)
 
 (define-key global-map [(control ?c) (?r)]		'revert-buffer-now)
 (define-key global-map [(control ?x) (?k)]		'kill-buffer-now)
