80cb22e69397fee6e81ae729d337f500930a3f1a
Cedric Lallain <kandjar@nausicaa>
2008-11-18 23:31:09 -0800

    [libs] Update log files
      Add logbuffer: an easy way to create a log file providing functionality to add text at the end of the buffer and autoscrolling

--- libs/klog.el
+++ libs/klog.el
@@ -12,23 +12,29 @@
 ;;
 ;;  12/06/06 - 12:06PM Log : My log file to test those features...
 ;;  --------------------------------------------------------------------------------
-;;  [info]  -> first log entry 
-;;  [info]  -> second log entry
-;;  [warn]  -> small issue
-;;  [errs]  -> a std error
-;;  [crit]  -> a major issue
-;;  [info] +-> entering a block
-;;  [info] |---> log within this block
-;;  [info] |---> second log within the same block
-;;  [info] +-> end of block
-;;  [info] +-> entering a block
-;;  [info] |---> log within this block
-;;  [info] |---> second log within the same block
-;;  [info] |-+-> second log within the same block
-;;  [info] | |--> second log within the same block
-;;  [info] | |--> second log within the same block
-;;  [info] | |--> second log within the same block
-;;  [info] |-+-> second log within the same block
-;;  [info] +-> end of block
+;;  [info] -> first log entry 
+;;  [info] -> second log entry
+;;  [warn] -> small issue
+;;  [errs] -> a std error
+;;  [crit] -> a major issue
+;;  [info] +> entering a block
+;;  [info] | -> log within this block
+;;  [info] | -> second log within the same block
+;;  [info] +> end of block
+;;  [errs] -> a std error
+;;  [crit] -> a major issue
+;;  [info] +> entering a block
+;;  [info] | -> log within this block
+;;  [info] | -> second log within the same block
+;;  [info] | +> second log within the same block
+;;  [info] | | -> second log within the same block
+;;  [info] | | -> second log within the same block
+;;  [info] | | -> second log within the same block
+;;  [info] | +> second log within the same block
+;;  [info] +> end of block
+;;  [todo] -> A todo item
+;;  [note] -> Just a comment
 
 
+;(defun klog-header(buffer title)
+;  (
