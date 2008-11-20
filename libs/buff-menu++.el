2a5a99b97ec28a3822c33447ca0b6d21e3742ab3
Cedric Lallain <kandjar@nausicaa>
2008-11-19 16:10:25 -0800

    [libs] Add buff-menu++ lib
      buff-menu++ is a adding some functionality to buff-menu:
       it provides an easy way to mark every files to save or to revert

---
+++ libs/buff-menu++.el
@@ -0,0 +1,119 @@
+;;
+;;
+;; Extra for the buffer mode:
+;;
+;; Didn't really check what (Buffer-menu-no-header) was doing... 
+;; The functions may required some 
+
+
+(defun Buffer-menu-mark-file-to-revert()
+  "Mark files to revert in the bufffer list"
+  (interactive)
+  (when (not (eq major-mode 'Buffer-menu-mode))
+      (error "Invalid Mode -- Expected: Buffer-menu-mode"))
+  (when (Buffer-menu-no-header)
+    (let ((buffer-read-only nil))
+      (delete-char 1)
+      (insert ?R)
+      (forward-line 1))))
+
+
+(defun Buffer-menu-mark-every-files-to-revert()
+  "Mark files to which can be reverted in the bufffer list"
+  (interactive)
+  (when (not (eq major-mode 'Buffer-menu-mode))
+      (error "Invalid Mode -- Expected: Buffer-menu-mode"))
+  (save-excursion
+    (let ((buffer-read-only nil))
+      (goto-char (point-min))
+      (while (not (eobp))
+	(let* ((buf (Buffer-menu-buffer t))
+	       (vis (verify-visited-file-modtime buf)))
+	  (when (not vis)
+	    (delete-char 1)
+	    (insert ?R))
+	(forward-line 1))))))
+
+(defun Buffer-menu-mark-every-files-to-save()
+  "Mark files to which can be reverted in the bufffer list"
+  (interactive)
+  (when (not (eq major-mode 'Buffer-menu-mode))
+      (error "Invalid Mode -- Expected: Buffer-menu-mode"))
+  (save-excursion
+    (let ((buffer-read-only nil))
+      (goto-char (point-min))
+      (while (not (eobp))
+	(let* ((buf (Buffer-menu-buffer t))
+	       (mod (buffer-modified-p buf))
+	       (wfi (save-excursion (set-buffer buf) buffer-file-name)))
+	  (when (and mod wfi)
+	    (beginning-of-line)
+	    (forward-char 2)
+	    (delete-char 1)
+	    (insert ?S))
+	(forward-line 1))))))
+
+;;================================================================================
+
+(defadvice Buffer-menu-unmark (around new-unmark-with-modified-flag (&optional backup))
+  (interactive "P")
+  (when (Buffer-menu-no-header)
+    (let* ((buf (Buffer-menu-buffer t))
+	   (mod (buffer-modified-p buf))
+	   (readonly (save-excursion (set-buffer buf) buffer-read-only))
+	   (buffer-read-only nil)
+	   (vis (verify-visited-file-modtime buf)))
+      (delete-char 3)
+      (insert ?\s
+	      (if readonly ?% ?\s)
+	      (if mod ?* (if vis ?\s ?#)))))
+  (forward-line (if backup -1 1)))
+
+(ad-activate 'Buffer-menu-unmark)
+
+;;================================================================================
+
+(defadvice list-buffers-noselect (after update-external-modification-flag (&optional files-only buffer-list))
+  (interactive)
+  (with-current-buffer (get-buffer "*Buffer List*")
+    (save-excursion
+      (let ((buffer-read-only nil))
+	(goto-char (point-min))
+	(while (not (eobp))
+	  (let* ((buf (Buffer-menu-buffer t))
+		 (mod (buffer-modified-p buf))
+		 (vis (verify-visited-file-modtime buf)))
+	    (forward-char 2) 
+	    (delete-char 1)
+	    (insert (if mod ?* (if vis ?\s ?#)))
+	    (beginning-of-line))
+	  (forward-line 1))))))
+
+(ad-activate 'list-buffers-noselect)
+
+;;================================================================================
+
+(defadvice Buffer-menu-execute (after revert-selected-files)
+  (save-excursion
+    (Buffer-menu-beginning)
+    (while (re-search-forward "^R" nil t)
+      (let ((modp nil)
+	    (rdp  nil)
+	    (visp nil))
+	(save-excursion
+	  (set-buffer (Buffer-menu-buffer t))
+	  (if buffer-file-name
+	      (revert-buffer t (not (buffer-modified-p))))
+	  (setq modp (buffer-modified-p))
+	  (setq rdp  buffer-read-only)
+	  (setq visp (verify-visited-file-modtime (current-buffer))))
+	(let ((buffer-read-only nil))
+	  (forward-char -1)
+	  (delete-char 3)
+	  (insert ?\s
+		  (if rdp ?% ?\s)
+		  (if modp ?* (if visp ?\s ?#)))
+	  )))))
+(ad-activate 'Buffer-menu-execute)
+
+(provide 'buff-menu++)
