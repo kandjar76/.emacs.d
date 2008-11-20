edf530982daab665079a0c6825ccdb5c1e76db4f
Cedric Lallain <kandjar@nausicaa>
2008-11-19 22:17:25 -0800

    [libs] Move c-tooltip from to-keep into libs

--- libs/c-tooltip.el
+++ libs/c-tooltip.el
@@ -1,3 +1,10 @@
+;;
+;; Function to use with tooltip-help.el
+;;  At work, man pages doesn't exist.
+;;  This function will search the function on the web.
+;;
+
+(provide 'c-tooltip)
 
 (defun th-c++-mode-handler()
   "Tooltip handler for C++"
@@ -9,9 +16,9 @@
 	(kill-buffer buf))
     (with-current-buffer (browse-url-emacs url)
       (let ((buffer-read-only nil))
-	(beginning-of-buffer)
+	(goto-char (point-min))
 	(search-forward "<pre>")
-	(next-line)
+	(forward-line 1)
 	(beginning-of-line)
 	(delete-region (point-min) (point))
 	(set-buffer-modified-p nil)
@@ -19,33 +26,33 @@
 	(beginning-of-line)
 	(delete-region (point) (point-max))
 	(set-buffer-modified-p nil)
-	(beginning-of-buffer)
+	(goto-char (point-min))
 	(while (search-forward-regexp "<[^>]*>"  nil t)
 	  (put-text-property (match-beginning 0)
 			     (match-end 0)
 			     'invisible t
 			     ))
-	(beginning-of-buffer)
+	(goto-char (point-min))
 	(while (search-forward-regexp "&lt;"  nil t)
 	  (put-text-property (match-beginning 0)
 			     (match-end 0)
 			     'display 
 			     "<"
 			     ))
-	(beginning-of-buffer)
+	(goto-char (point-min))
 	(while (search-forward-regexp "&gt;"  nil t)
 	  (put-text-property (match-beginning 0)
 			     (match-end 0)
 			     'display 
 			     ">"
 			     ))
-	(beginning-of-buffer)
+	(goto-char (point-min))
 	(while (search-forward ""  nil t)
 	  (put-text-property (match-beginning 0)
 			     (match-end 0)
 			     'invisible t 
 			     ))
-	(beginning-of-buffer)
+	(goto-char (point-min))
 	(set-buffer-modified-p nil))
       )
     "")
