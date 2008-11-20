2b3dc45a9fb0830829040dcab3b83a02bff17fe5
Cedric Lallain <kandjar@nausicaa>
2008-11-19 22:25:51 -0800

    [modes] Remove autocompletion associated to the tab key for the awk-mode

--- modes/my-awk.el
+++ modes/my-awk.el
@@ -2,19 +2,9 @@
 ;; Setup a Specific identation for awk script:
 ;;
 
-(defun my-awk-smart-tab (&optional ARG)
-  "Intelligently decides whether to indent or do word-completion."
-  (interactive "P")
-  ;; Special case: a region has been selected -- run the alignment on the selected region
-  (if mark-active
-      (c-indent-line-or-region)
-      (c-indent-command ARG))
-)
-
-(defun my-awk-setup ()
+(defun custom-awk-setup ()
   ;;(c-set-style "gnu")
   (setq tab-width 4)
-  (local-set-key [(tab)] 'my-awk-smart-tab)
   (c-set-offset 'substatement 4 nil)
   (c-set-offset 'statement-block-intro 4 nil)
   (c-set-offset 'substatement-open 0 nil)
@@ -27,4 +17,4 @@
   ;(modify-syntax-entry ?\n "> b"   awk-mode-syntax-table)
 )
 
-(add-hook 'awk-mode-hook 'my-awk-setup)
+(add-hook 'awk-mode-hook 'custom-awk-setup)
