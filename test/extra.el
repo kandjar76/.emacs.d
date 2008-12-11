1047d918e98ca8b6251eb24dae91ac6a4792acc9
Cedric Lallain <clallain@glacier2-dog.naughtydog.com>
2008-12-10 19:58:17 -0800

    [test] Added sample code: to add subdir to load-path and to locate the loaded library + Animated string test

--- test/extra.el
+++ test/extra.el
@@ -13,3 +13,19 @@ http://www.google.com
 (setq button-url-regexp
       "\\([-a-zA-Z0-9_=!?#$@~`%&*+\\/:;,]+\\.\\)+[-a-zA-Z0-9_=!?#$@~`%&*+\\/:;,]*[-a-zA-Z0-9\\/]")
 (bounds-of-thing-at-point 
+
+
+;; Including load-path subdirectories:
+ (setq load-path (cons my-lisp-dir load-path))
+ (normal-top-level-add-subdirs-to-load-path)
+
+;; Including specific load-path subd:
+(normal-top-level-add-to-load-path
+ '("emms" "erc" "planner" "w3"))
+
+;; Finding library:
+M-x locate-library
+M-x list-load-path-shadows
+
+;; Fun: 
+(animate-string "fun fun fun!!!" <line> <col>)
