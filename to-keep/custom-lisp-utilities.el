68ebac54c5ce78ab6f5b32ca0b89a0936e5f37f0
Cedric Lallain <kandjar@Camelia>
2009-01-04 13:10:45 -0800

    [to-keep] Code to add time to the modeline, and ffa's advise to reach the specified line (using the syntax file:line)

--- to-keep/custom-lisp-utilities.el
+++ to-keep/custom-lisp-utilities.el
@@ -17,3 +17,14 @@
 	     (substring string begpos endpos))
 	"")))
 
+;; Go to the indicated line with ‘find-file-at-point’:
+(defadvice find-file-at-point (around goto-line compile activate)
+  (let ((line (and (looking-at ".*:\\([0-9]+\\)")
+		   (parse-integer (match-string 1)))))
+    ad-do-it
+    (and line (goto-line line))))
+
+;; Display the Time in the modeline
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+(display-time)
