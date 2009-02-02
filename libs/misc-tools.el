1d6e0d6433daee01d692b3f6235f8ac874b2e59f
Cedric Lallain <kandjar@camelia.(none)>
2009-02-01 19:46:26 -0800

    [misc] Added advice to show the line of the matching non-visible parenthesis

--- libs/misc-tools.el
+++ libs/misc-tools.el
@@ -42,6 +42,23 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+(defadvice show-paren-function (after show-matching-paren-offscreen
+                                      activate)
+  "If the matching paren is offscreen, show the matching line in the                               
+echo area. Has no effect if the character before point is not of                                   
+the syntax class ')'."
+  (interactive)
+  (let ((matching-text nil))
+    ;; Only call `blink-matching-open' if the character before point                               
+    ;; is a close parentheses type character. Otherwise, there's not                               
+    ;; really any point, and `blink-matching-open' would just echo                                 
+    ;; "Mismatched parentheses", which gets really annoying.                                       
+    (if (char-equal (char-syntax (char-before (point))) ?\))
+        (setq matching-text (blink-matching-open)))
+    (if (not (null matching-text))
+        (message matching-text))))
+
+
 ;;;###autoload
 (defun electric-split-window-horizontally(&optional arg)
   "Split current window into two window side by side, move the cursor to the new created window"
