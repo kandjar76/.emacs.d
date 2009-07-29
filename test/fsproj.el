a358fcb805a381d98cf4fc009f2224d241bed192
Cedric Lallain <clallain@glacier2-dog.naughtydog.com>
2009-07-29 10:27:08 -0700

    [fsproj] Update todo list + WIP: Generate Project Name

--- test/fsproj.el
+++ test/fsproj.el
@@ -9,8 +9,8 @@
 ;; TODO
 ;;
 ;; - [x] Extract the whole file list
-;; - [ ] Detect the projects
-;; - [ ] Create a list of files associated to their project 
+;; - [X] Detect the projects
+;; - [X] Create a list of files associated to their project 
 ;; - [ ] Keep the relative path based on the project 'root' folder
 ;; - [ ] Remap the files
 ;; - [ ] Create the project buffer window
@@ -78,7 +78,7 @@ Note: the project list is sorted in descending alphabetic order."
 	    (when pos
 	      (setq proj-list (cons (file-name-directory (substring fullpath 0 pos)) proj-list)))
 	  )))))
-    (list (reverse (sort proj-list 'string-lessp)) file-list)))
+    (list (sort proj-list 'string-lessp) file-list)))
 
 
 (defun fsproj-extract-project-file-list(current-project file-list)
@@ -93,6 +93,110 @@ Return a list of two lists: ((current project file list..) (remaining files...).
 	    (setq project-file-list (cons current-file project-file-list))
 	    (setq remaining-files (cons current-file remaining-files)))))
     (list project-file-list remaining-files)))
+
+
+(defun fsproj-parent-of-p(child parent)
+  "Check if CHILD is a child of the directory PARENT.
+CHILD and PARENT are two string representing directories."
+  (let* ((clist (and child  (split-string child "/")))
+	 (plist (and parent (split-string parent "/")))
+	 (cont t)
+	 res)
+    (while (and clist plist cont)
+      (let ((cname (pop clist))
+	    (pname (pop plist)))
+	(setq cont (string-equal cname pname))))
+    (and cont (null plist))))
+  
+
+(defun fsproj-resolve-conflict(conflict-list)
+  "Solve the CONFLICT-LIST and return the list of final names."
+  (let* ((path-list (mapcar (lambda (node) (reverse (split-string (car node) "/"))) conflict-list))
+	 (subp-list (mapcar (lambda (node) (cdr node)) conflict-list))
+	 (cleared-list (apply 'mapcar*
+			      (lambda (&rest lst)  (or (reduce (lambda (v1 v2) (and (string-equal v1 v2) v1)) lst) lst))
+			      splitted-list))
+	 (number-of-conflict (length conflict-list))
+	 (;; let's for a list of items: ("current-name" "subproj), then we'll go through each node of cleared-list to test.
+	  ;; once' a conflict is solved, the list becomes a string! number of conflict decreased. once = 0 -> we leave ;-)
+	 ))
+    ;; cleared-list is a list of node; each node are either a string, or a list of string.
+    ;; '(("a" "b" "c") ("1" "2" "c") ("A" "B" "c")) -> '(("a" "1" "A") ("b" "2" "B") "c")
+    ;; note: each subproj can be nil or "sub1/sub2/..." or just "sub"
+))
+
+
+
+
+(defun fsproj-generate-project-names(project-list)
+  "Return a list of project names based on the project paths contained in PROJECT-LIST.
+Making sure each name is uniq. This function will also detect subproject and add the master project name as prefix."
+  (let ((project-base-list (mapcar (lambda (path) (substring path 0 -1)) project-list))
+	(project-name-list (mapcar (lambda (path) (file-name-nondirectory (substring path 0 -1))) project-list))
+	(project-ht (make-hash-table :test 'equal))
+	subproject-list)
+
+    ;; Extract the subproject list:
+    (let ((path-list project-base-list)
+	  subprojects
+	  sub-list)
+      (while path-list
+	(let ((current (pop path-list))
+	      subproj)
+	  (while (and (not subproj) subprojects)
+	    (if (fsproj-parent-of-p current (cdr (car subprojects)))
+		(setq subproj     (car (car subprojects))
+		      subprojects (cons (cons (concat (car (car subprojects)) "/" (file-name-nondirectory current)) 
+					      current)
+					subprojects))
+		(setq subprojects (cdr subprojects))))
+	  (when (not subprojects)
+	    (setq subprojects (list (cons (file-name-nondirectory current) current))))
+	  (setq sub-list (cons subproj sub-list))))
+      ;;
+      (setq subproject-list (reverse sub-list)))
+
+    ;; 
+
+    ;; Build the hash table:
+    ;; each node of the hash table will be initially be a list of : '("basepath" "subproj")
+    ;; Note: subproj can be nil. 
+
+    ;; First path: initialization of the hash table throught the list: 
+    (let ((name-list project-name-list)
+	  (base-list project-base-list)
+	  (sub-list  subproject-list))
+      (while base-list
+	(let ((cur-name (pop name-list))
+	      (cur-base (pop base-list))
+	      (cur-subp (pop sub-list)))
+	  (puthash cur-name 
+		   (cons (list cur-base cur-subp)
+			 (gethash cur-name project-ht)) 
+		   project-ht))))
+
+    ;; The second path will solve the conflicts and patch theses values.
+    ;; Each node is a list of '("basepath" "subproj") and will be converted
+    ;; into a list a string corresponding to the final name for each project
+    ;; Note: the initial list has been build in reverse order
+    (let ((name-list project-name-list))
+      (while name-list
+	(let* ((cur-name (pop name-list))
+	       (cur-node (gethash cur-name project-ht)))
+	  (when (listp (car cur-node))
+	    (puthash cur-name (fsproj-resolve-conflict cur-node) project-ht)))))
+
+    ;; The third will retrieve the conflict-less name
+    (let ((name-list project-name-list)
+	  reversed-list)
+      (while name-list
+	(let* ((cur-name (pop name-list))
+	       (cur-node (gethash cur-name project-ht)))
+	  (setq reversed-list (cons (pop cur-node) reversed-list))
+	  (puthash cur-name cur-node project-ht)))
+      (reverse reversed-list))
+))
+
 
 
 
